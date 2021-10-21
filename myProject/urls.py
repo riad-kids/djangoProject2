4918d41ee1d6832ee56f633a9b4d5b1518b24fff
riad <riad.k@kidsnote.com>
2021-10-21 16:09:20 +0900

    part 5

--- myProject/urls.py
+++ myProject/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
 
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
 
     path('signup/', accounts_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
